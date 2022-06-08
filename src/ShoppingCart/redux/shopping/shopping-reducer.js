1a71b84a1b0f6889b297f4e3c68c97ec4ce2566f
MdTasib <ohidulalam442@gmail.com>
2022-06-08 10:48:53 +0600

    shopping cart reducer implement

--- src/ShoppingCart/redux/shopping/shopping-reducer.js
+++ src/ShoppingCart/redux/shopping/shopping-reducer.js
@@ -42,13 +42,43 @@ const initialState = {
 const shopReducer = (state = initialState, action) => {
 	switch (action.type) {
 		case ADD_TO_CART:
-			return {};
+			// get the item data from the products array
+			const item = state.products.find(
+				product => product.id === action.payload.id
+			);
+			// check item is already added in cart - return ture or false
+			const inCart = state.cart.find(product =>
+				product.id === action.payload.id ? true : false
+			);
+
+			return {
+				...state,
+				cart: inCart
+					? state.cart.map(item =>
+							item.id === action.payload.id
+								? { ...item, qty: item.qty + 1 }
+								: item
+					  )
+					: [...state.cart, { ...item, qty: 1 }],
+			};
 		case REMOVE_FROM_CART:
-			return {};
+			return {
+				...state,
+				cart: state.cart.filter(item => item.id !== action.payload.id);
+			};
 		case ADJUST_QTY:
-			return {};
+			return {
+				...state,
+				cart: state.cart.map(item => item.id === action.payload.id 
+					? {...item, qty: action.payload.qty} 
+					: item
+				)
+			};
 		case LOAD_CURRENT_ITEM:
-			return {};
+			return {
+				...state,
+				currentItem: action.payload
+			};
 		default:
 			return state;
 	}
