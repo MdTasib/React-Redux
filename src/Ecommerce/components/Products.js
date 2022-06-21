7b7f62f85b1d07b48d7ef9852643e6010f5507be
MdTasib <ohidulalam442@gmail.com>
2022-06-21 15:55:33 +0600

    product section desing implement

--- src/Ecommerce/components/Products.js
+++ src/Ecommerce/components/Products.js
@@ -37,7 +37,33 @@ const Products = () => {
 				<button className='btn btn-outline-dark me-2 w-100'>JEWELERY</button>
 				<button className='btn btn-outline-dark me-2 w-100'>ELECTRONIC</button>
 			</div>
-			{loading ? <Loading /> : <p>{filter.lenght}</p>}
+			{loading ? (
+				<Loading />
+			) : (
+				<div className='row g-3 pt-5'>
+					{filter.map(product => (
+						<div className='col-md-3'>
+							<div class='card h-100 text-center border-0 shadow-sm'>
+								<img
+									src={product.image}
+									class='card-img-top'
+									height={250}
+									alt={product.title}
+								/>
+								<div class='card-body'>
+									<h5 class='card-title'>
+										{product.title.substring(0, 12)}...
+									</h5>
+									<p class='card-text fw-bold'>$ {product.price}</p>
+									<a href='#' class='btn btn-outline-dark'>
+										BUY NOW
+									</a>
+								</div>
+							</div>
+						</div>
+					))}
+				</div>
+			)}
 		</div>
 	);
 };
